72186e6367b0ebd68eb49d1ae11276c6758c68b2
Joshua Katuka <Josh@Joshuas-Air-010010054105.app.byu.edu>
2016-11-15 13:25:01 -0700

    Few more changes

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -19,6 +19,7 @@ angular.module('carsale', [])
       return $http.put('/cars/' + car._id + '/upvote').success(function(data){
         console.log("upvote worked");
         car.upvotes = data.upvotes;
+        car.upvotes += 1;
       });
     };
 
